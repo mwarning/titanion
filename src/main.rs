798ca5eca0e80f95990da1f9d2bdefa109ffaaa3
Moritz Warning <moritzwarning@web.de>
2017-02-10 21:01:36 +0100

    add main module

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+
+#![allow(dead_code)]
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(non_upper_case_globals)]
+
+
+mod util {
+	pub mod vector;
+	pub mod sdl {
+		pub mod displaylist;
+	}
+}
+
+mod ttn {
+	pub mod shape;
+}
+
+
+fn main() {
+	//call boot...
+}
